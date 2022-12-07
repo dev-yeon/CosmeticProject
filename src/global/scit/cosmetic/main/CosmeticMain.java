ac09f2ab1c4745bd42e863b6decc21eb65645bf3
dev-yeon <nochi@kakao.com>
2022-12-08 01:51:12 +0900

    'ttt'

--- src/global/scit/cosmetic/main/CosmeticMain.java
+++ src/global/scit/cosmetic/main/CosmeticMain.java
@@ -3,7 +3,7 @@ package global.scit.cosmetic.main;
 public class CosmeticMain {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello!!!");
 	}
 
 }
